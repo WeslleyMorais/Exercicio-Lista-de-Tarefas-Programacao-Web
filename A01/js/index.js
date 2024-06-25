9bdbcad411e5b2f8dd0482cf72e2ae8504db5840
WeslleyMorais <166230826+WeslleyMorais@users.noreply.github.com>
2024-06-25 19:02:53 -0300

    A16

--- A01/js/index.js
+++ A01/js/index.js
@@ -1,1 +1,22 @@
-alert('Alert de um arquivo JS externo!')
+let tempo
+
+function ativarContagem(){
+
+   tempo = setInterval(function(){
+    var cronometro = document.getElementById('tempo').innerHTML;
+    var soma = parseInt(cronometro) +1;
+        document.getElementById('tempo').innerHTML = soma
+       
+        if(soma >= 1){
+            const botao = document.getElementById('start')
+            botao.textContent = "Continuar"
+            botao.style.backgroundColor = 'blue'
+        
+        }
+    }, 1000)
+    
+}
+
+function pararContagem(){
+    clearInterval(tempo);
+}
